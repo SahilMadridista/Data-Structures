193e1f93d283ae3bc3bdb4837749b455e70c7d79
sahil <sahil91098singh@gmail.com>
2021-05-30 21:22:12 +0530

    Comments added.

--- Vector/src/vector/MainClass.java
+++ Vector/src/vector/MainClass.java
@@ -6,6 +6,12 @@ public class MainClass {
 
 	public static void main(String[] args) {
 		
+		// Same as arraylist.
+		// It locks the operation.
+		// Same functions as arraylist.
+		// It takes more memory than arraylist.
+		// Vectors are thread safe but less efficient.
+		
 		Vector<Integer> v = new Vector<>();
 		
 		for(int i = 1;i<11;i++) {
