59b84c3c6ede5b8e83975e82ea3a2d9fb6f8e8aa
sahil <sahil91098singh@gmail.com>
2021-06-04 21:42:52 +0530

    Roman to integer completed.

--- LeetcodeProblems/src/easy/RomanToInteger.java
+++ LeetcodeProblems/src/easy/RomanToInteger.java
@@ -12,8 +12,26 @@ public class RomanToInteger {
 		romanMap.put('I', 1);
 		romanMap.put('V', 5);
 		romanMap.put('X', 10);
+		romanMap.put('L', 50);
+		romanMap.put('C', 100);
+		romanMap.put('D', 500);
+		romanMap.put('M', 1000);
+
+		String str = "IX"; // value is 29
 		
-		System.out.println(romanMap);
+		int value = 0;
+		
+		for(int i=0;i<str.length();i++) {
+			
+			if(i>0 && romanMap.get(str.charAt(i)) > romanMap.get(str.charAt(i-1))){	
+				value += romanMap.get(str.charAt(i)) - 2*romanMap.get(str.charAt(i-1));
+			}else {
+				value += romanMap.get(str.charAt(i));
+			}
+			
+		}
+		
+		System.out.println(value);
 		
 	}
 
