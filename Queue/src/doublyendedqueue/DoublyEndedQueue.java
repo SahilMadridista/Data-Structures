d082a1757bfaf3d81f82c68c88194c4fc76e9824
sahil <sahil91098singh@gmail.com>
2021-06-08 21:50:41 +0530

    1 file added.

--- Queue/src/doublyendedqueue/DoublyEndedQueue.java
+++ Queue/src/doublyendedqueue/DoublyEndedQueue.java
@@ -1,7 +1,26 @@
 package doublyendedqueue;
 
+import java.util.ArrayDeque;
+
 public class DoublyEndedQueue {
 	
-	
+	public static void main(String[] args) {
+		// Code goes here
+		
+		ArrayDeque<Integer> ad = new ArrayDeque<>();
+		ad.addFirst(2);
+		ad.addLast(5);
+		
+		System.out.println(ad.peekFirst());
+		
+		System.out.println(ad.peekLast());
+		
+		ad.removeFirst();
+		
+		System.out.println(ad.getFirst());
+		
+		System.out.println(ad.getLast());
+		
+	}
 
 }
