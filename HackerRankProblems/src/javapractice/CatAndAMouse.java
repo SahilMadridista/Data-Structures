d250288524153f6937a5643c473d622b10906e48
sahil <sahil91098singh@gmail.com>
2021-06-21 05:00:04 +0530

    Cat and a mouse problem done.

---
+++ HackerRankProblems/src/javapractice/CatAndAMouse.java
@@ -0,0 +1,17 @@
+package javapractice;
+
+public class CatAndAMouse {
+
+	static String catAndMouse(int x, int y, int z) {
+
+		if(Math.abs(z-x)>Math.abs(z-y)) {
+            return "Cat B";
+        }else if(Math.abs(z-x)<Math.abs(z-y)) {
+            return "Cat A";
+        }else {
+            return "Mouse C";
+        }
+
+    }
+	
+}
