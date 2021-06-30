15f130b6632b67af6bcc657aa94431f5ffda9bcf
sahil <sahil91098singh@gmail.com>
2021-06-30 15:16:12 +0530

    Tree traversal ( in, pre and post ) implemented.

--- Tree/src/tree/MyTree.java
+++ Tree/src/tree/MyTree.java
@@ -7,7 +7,13 @@ public class MyTree {
 	static Scanner scan = null;
 	
 	public static void main(String[] args) {
-		createTree();
+		Node root = createTree();
+		System.out.println();
+		inOrder(root);
+		System.out.println();
+		preOrder(root);
+		System.out.println();
+		postOrder(root);
 	}
 	
 	static Node createTree() {
@@ -27,6 +33,41 @@ public class MyTree {
 		root.right = createTree(); // -1 null
 		
 		return root;
+		
+	}
+	
+	static void inOrder(Node root) {
+		
+		// Left - Node - Right
+		
+		if(root == null) return;
+		inOrder(root.left);
+		System.out.print(root.data + " ");
+		inOrder(root.right);
+		
+	}
+	
+	static void preOrder(Node root) {
+		
+		// Node - Left - Right
+		
+		if(root == null) return;
+		
+		System.out.print(root.data + " ");
+		preOrder(root.left);
+		preOrder(root.right);
+		
+	}
+	
+	static void postOrder(Node root) {
+		
+		// Left - Right - Node
+		
+		if(root == null) return;
+		
+		postOrder(root.left);
+		postOrder(root.right);
+		System.out.print(root.data + " ");
 		
 	}
 	
