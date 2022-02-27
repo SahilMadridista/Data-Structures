cdafd793a6c2c8dc7c7278083153316d6b4bdc0e
sahil <sahil91098singh@gmail.com>
2022-02-27 20:33:09 +0530

    Insertion sort implemented and explained.

--- Algorithms/Sortings/InsertionSort.java
+++ Algorithms/Sortings/InsertionSort.java
@@ -5,22 +5,16 @@ public class InsertionSort {
       int n = arr.length;
 
       for(int i = 1;i<n;i++){
-
          int temp = arr[i];
-
          int j = i-1;
          while(j>=0 && arr[j]>temp){
             arr[j+1] = arr[j];
             j--;
          }
-
          arr[j+1] = temp;
+      }     
 
-      }
-      
-      
       System.out.println("Sorted array is: ");
-
       for(int i = 0;i<arr.length;i++){
          System.out.println(arr[i] + " ");
       }
@@ -28,3 +22,10 @@ public class InsertionSort {
    }
    
 }
+
+// Array gets divided into 2 parts.
+// Sorted and Unsorted.
+// We pick the first element and that element alone is sorted and rest of the array is unsorted.
+// So, from the unsorted array, we pick the first element and put it in temp variable.
+// we compare the temp with the sorted array from last to first element.
+// And we do swap accordingly.
