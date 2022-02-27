81c019772d703299e1f8ec9186adfad9cc337923
sahil <sahil91098singh@gmail.com>
2022-02-27 19:24:48 +0530

    Bubble sort further optimized with checing the swap

--- Algorithms/Sortings/BubbleSort.java
+++ Algorithms/Sortings/BubbleSort.java
@@ -14,14 +14,21 @@ class BubbleSort{
 
       for(int i = 0;i<arr.length;i++){
 
+         boolean swapped = false;
+
          for(int j = 0;j<arr.length-i-1;j++){
 
             if(arr[j+1] < arr[j]){
+               swapped = true;
                int temp = arr[j+1];
                arr[j+1] = arr[j];
                arr[j] = temp;
             }
 
+         }
+
+         if(!swapped){
+            break;
          }
 
       }
