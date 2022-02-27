0264f72e98f40952e19fae2bf28ae2d181b1842b
sahil <sahil91098singh@gmail.com>
2022-02-27 20:22:50 +0530

    Insertion sort implemented.

--- Algorithms/Sortings/InsertionSort.java
+++ Algorithms/Sortings/InsertionSort.java
@@ -1,5 +1,30 @@
 public class InsertionSort {
 
-   
+   public static void main(String[] args) {
+      int arr[] = new int[]{8,4,1,5,9,2};
+      int n = arr.length;
+
+      for(int i = 1;i<n;i++){
+
+         int temp = arr[i];
+
+         int j = i-1;
+         while(j>=0 && arr[j]>temp){
+            arr[j+1] = arr[j];
+            j--;
+         }
+
+         arr[j+1] = temp;
+
+      }
+      
+      
+      System.out.println("Sorted array is: ");
+
+      for(int i = 0;i<arr.length;i++){
+         System.out.println(arr[i] + " ");
+      }
+
+   }
    
 }
