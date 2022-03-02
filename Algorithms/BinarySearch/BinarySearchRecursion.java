7c476db95ec28e6da8ad2236e4502e71ca27d348
sahil <sahil91098singh@gmail.com>
2022-03-03 01:35:50 +0530

    Recursive binary search implemented.

--- Algorithms/BinarySearch/BinarySearchRecursion.java
+++ Algorithms/BinarySearch/BinarySearchRecursion.java
@@ -1,3 +1,34 @@
 public class BinarySearchRecursion {
    
+   public static void main(String[] args) {
+      int arr[] = new int[]{-4,-1,3,7,10,12};
+      int k = 3;
+      int n = arr.length;
+      int keyPosition = binarySearch(arr,k,0,n-1);
+      if(keyPosition == -1){
+         System.out.println(k + " is not present in the array");
+      }else{
+         System.out.println(k + " is present at position " + keyPosition);
+      }
+      
+   }
+
+   static int binarySearch(int a[],int key,int low, int high){
+
+      if(low>high){
+         return -1;
+      }
+      int mid = (low+high)/2;
+
+      if(a[mid]==key){
+         return mid;
+      }
+
+      if(a[mid]>key){
+         return binarySearch(a,key,low,mid-1);
+      }else{
+         return binarySearch(a, key, mid+1, high);
+      }
+
+   }
 }
