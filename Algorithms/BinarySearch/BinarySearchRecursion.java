b05b8ab2f51324d4b697e9d79c8f103142e00df2
sahil <sahil91098singh@gmail.com>
2022-03-03 22:34:25 +0530

    Search element in infinite sorted array implemented in recursion binary search.

--- Algorithms/BinarySearch/BinarySearchRecursion.java
+++ Algorithms/BinarySearch/BinarySearchRecursion.java
@@ -31,4 +31,18 @@ public class BinarySearchRecursion {
       }
 
    }
+
+   static int searchInfinite(int a[],int key){
+      
+      int low = 0;
+      int high = 1;
+
+      while(a[high]<key){
+         low = high;
+         high = 2*high;
+      }
+
+      return binarySearch(a, key, low, high);
+
+   }
 }
