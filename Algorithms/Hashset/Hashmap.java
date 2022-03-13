49cfb3b3a394e2dfc92166c8f44d1bf2f03de6f7
sahil <sahil91098singh@gmail.com>
2022-03-13 07:35:22 +0530

    Distinct elements in specific window implemented using hashmap.

---
+++ Algorithms/Hashset/Hashmap.java
@@ -0,0 +1,32 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Hashmap{
+
+   public static void main(String[] args) {
+      
+      int a[] = new int[]{1,2,2,1,3,1,1,3};
+      int k = 4;
+      int n = a.length;
+      Map<Integer,Integer> map = new HashMap<>();
+      for(int i = 0;i<k;i++){
+         map.put(a[i],map.getOrDefault(a[i],0 +1));
+      }
+      System.out.println(map.size());
+
+      for(int i=k;i<n;i++){
+ 
+         if(map.get(a[i-k])==1){
+            map.remove(a[i-k]);
+         }else{
+            map.put(a[i-k], map.get(a[i-k])-1);
+         }
+
+         map.put(a[i],map.getOrDefault(a[i],0)+1);
+         System.out.println(map.size());
+
+      }
+
+   }
+
+}
