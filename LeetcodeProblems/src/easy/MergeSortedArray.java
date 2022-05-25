adcfb0ef1b7b7ea728b89478fd4e58db84e4ff03
sahil <sahil91098singh@gmail.com>
2022-05-25 22:34:59 +0530

    Merge sorted array done.

---
+++ LeetcodeProblems/src/easy/MergeSortedArray.java
@@ -0,0 +1,19 @@
+package easy;
+
+import java.util.Arrays;
+
+public class MergeSortedArray {
+
+	public void merge(int[] nums1, int m, int[] nums2, int n) {
+        
+        int count=0;
+        for(int i=m;i<m+n;i++){
+            nums1[i] = nums2[count++];
+        }
+        Arrays.sort(nums1);
+        
+        // We can use any sorting algorithm here too
+        
+    }
+	
+}
