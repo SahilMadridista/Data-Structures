d5d4f20726fab1ea29f3afe16efd6bb9300cfd3b
sahil <sahil91098singh@gmail.com>
2022-05-26 23:40:48 +0530

    New node class added in medium problems.

---
+++ LeetcodeProblems/src/medium/MergeNodesBetweenZeros.java
@@ -0,0 +1,103 @@
+package medium;
+
+public class MergeNodesBetweenZeros {
+	
+	public Node mergeNodes(Node head) {
+		
+		
+		Node node = head;
+        while(node != null)
+        { 
+            int sum = 0;
+            if(node.data == 0){
+                
+                Node temp = node.next;
+                while(temp != null && temp.data != 0){
+                    sum += temp.data;
+                    temp = temp.next;
+                }
+                node.data = sum;
+                if(temp != null && temp.next != null){
+                    node.next = temp;
+                }else{
+                     node.next = null;
+                }
+            }
+            
+            node = node.next;
+        }
+        
+        return head;
+		
+		
+		
+		// Alternate solutions in which time limit was exceeding
+		
+		/* 
+		  
+		------------1st Solution------------
+		
+		Node temp = head.next;
+	    
+	    
+	    while(temp.next != null){
+	        
+	        int dataue = 0;
+	        
+	        if(temp.data != 0){
+	            dataue = dataue + temp.data;
+	        }else{
+	            Node new_node = new Node(dataue);
+	            head.next = new_node;
+	            new_node.next = temp;
+	            dataue = 0;
+	        }
+	    }
+	    
+	    if(head.data == 0){
+	        head = head.next;
+	    }
+	    
+	    Node temp2 = head;
+	    while(temp2.next.next != null){
+	        if(temp2.next.data == 0){
+	            temp2.next = temp2.next.next;
+	        }
+	    }
+	    
+	    return head;
+	    
+	    
+	    -------------2nd Solution------------
+	    
+	    ListNode temp = head.next;
+        if(temp.val == 0){
+            temp=temp.next;
+        }
+        
+        while(temp.next != null){
+            int value = 0;
+            
+            if(temp.val != 0){
+                value += temp.val;
+            }else{
+                ListNode new_node = new ListNode(value);
+                head.next = new_node;
+                new_node.next = temp.next;
+                value = 0;
+            }
+            
+        }
+        
+        return head.next;
+	    
+		
+		*/
+		
+	}
+	
+	
+	
+	
+
+}
